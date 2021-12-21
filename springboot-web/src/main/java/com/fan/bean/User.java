6355be1354250c88e33263545ea707e20b0eac28
coolerpluto <8039035008@qq.com>
2021-12-21 23:01:18 +0800

    1

---
+++ springboot-web/src/main/java/com/fan/bean/User.java
@@ -0,0 +1,9 @@
+package com.fan.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
--- springboot-web/src/main/java/com/fan/controller/IndexController.java
+++ springboot-web/src/main/java/com/fan/controller/IndexController.java
@@ -1,8 +1,12 @@
 package com.fan.controller;
 
+import com.fan.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
@@ -11,7 +15,25 @@ public class IndexController {
         return "login";
     }
     @PostMapping("/login")
-    public String mainPage(){
-        return "index";
+    public String main(User user, HttpSession session){
+        if(!StringUtils.isEmpty(user.getUserName()) && StringUtils.hasLength(user.getPassword())){
+            session.setAttribute("userStatus", "true");
+            session.setMaxInactiveInterval(20);
+            return "redirect:main.html";
+        }else {
+            return "login";
+        }
+
+    }
+
+    @GetMapping("/main.html")
+    public String mainPage(HttpSession session){
+        System.out.println(session.getAttribute("userStatus"));
+        if (session.getAttribute("userStatus")!=null){
+            return "main";
+        }else {
+            return "login";
+        }
+
     }
 }
