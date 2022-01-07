a7013a59874013d5dbf78ddbc0f4b23669eb66d5
coolerpluto <839035008@qq.com>
2022-01-07 16:55:25 +0800

    1

---
+++ springboot-web/src/main/java/com/fan/config/AdminWebConfig.java
@@ -0,0 +1,15 @@
+package com.fan.config;
+
+import com.fan.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class AdminWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/","/login");
+    }
+}
--- springboot-web/src/main/java/com/fan/interceptor/LoginInterceptor.java
+++ springboot-web/src/main/java/com/fan/interceptor/LoginInterceptor.java
@@ -1,9 +1,32 @@
 package com.fan.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
-    pre
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object loginUser = session.getAttribute("loginUser");
+        if(loginUser != null){
+            return true;
+        }
+        response.sendRedirect("/");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
 }
