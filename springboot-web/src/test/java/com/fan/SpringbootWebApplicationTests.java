d5ef51f97b33390dabd9580567f02164a822717b
coolerpluto <8039035008@qq.com>
2022-02-23 22:24:53 +0800

    `1`

--- springboot-web/src/test/java/com/fan/SpringbootWebApplicationTests.java
+++ springboot-web/src/test/java/com/fan/SpringbootWebApplicationTests.java
@@ -5,18 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 @Slf4j
 @SpringBootTest
 class SpringbootWebApplicationTests {
     @Autowired
     JdbcTemplate jdbcTemplate;
-
+    @Autowired
+    DataSource dataSource;
 
 
     @Test
     void contextLoads() {
         Long aLong = jdbcTemplate.queryForObject("select count(1) from student", Long.class);
         log.info("student表有{}条数据", aLong);
+        log.info("数据源类型:{}",dataSource.getClass());
     }
 
 }
